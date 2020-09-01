1a8f5c590b3634b2d89f1a76eafd1270eb9b5a9c
mohamedisakr <mohamedisakr@gmail.com>
2020-09-01 10:28:35 +0200

    implemeting switch to alert

--- 1-fundamentals/scroll/test/ScrollToElement.java
+++ 1-fundamentals/scroll/test/ScrollToElement.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.interactions.Actions;
 public class ScrollToElement {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium-Webdriver\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
--- 1-fundamentals/switch-window/test/SwitchToActiveWindow.java
+++ 1-fundamentals/switch-window/test/SwitchToActiveWindow.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SwitchToActiveWindow {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium-Webdriver\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
